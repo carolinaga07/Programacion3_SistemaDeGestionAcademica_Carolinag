245b517d0951ae4b04b67a08c63027584410e643
carolina <carolina@MONNIE>
2026-05-29 12:42:00 -0400

    finalizacion de clases calificacion y materia, dando asi final a todas las clases bases

--- src/Calificacion.java
+++ src/Calificacion.java
@@ -1,3 +1,31 @@
 public class Calificacion {
+
+    private Materia materia;
+    private double Nota;
+   
+    public Calificacion(Materia materia, double nota) {
+        this.materia = materia;
+        this.Nota = nota;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return Nota;
+    }
+
+    public void setNota(double nota) {
+        Nota = nota;
+    }
+
+    
+
+    
     
 }
--- src/Materia.java
+++ src/Materia.java
@@ -1,5 +1,50 @@
-public class Materia extends Profesor {
+public class Materia  {
+    private int Codigo;
+    private String Nombre;
     private int cantidadCreditos;
+    
+    public Materia(int codigo, String nombre, int cantidadCreditos) {
+        this.Codigo = codigo;
+        this.Nombre = nombre;
+        this.cantidadCreditos = cantidadCreditos;
+    }
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        Codigo = codigo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getCantidadCreditos() {
+        return cantidadCreditos;
+    }
+
+    public void setCantidadCreditos(int cantidadCreditos) {
+        this.cantidadCreditos = cantidadCreditos;
+    }
+
+    public void MostrarInfo(){
+        System.out.println("========================");
+        System.out.println("       INFORMACION:     ");
+        System.out.println("========================");
+        System.out.println("Nombre:" + getNombre());
+        System.out.println("Codigo:" + getCodigo());
+        System.out.println("Creditos:" + getCantidadCreditos());
+    }
+
+    
+
+
 
     
 
