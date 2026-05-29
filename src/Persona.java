c12e587daa8fbc48b71facab2c70c3f4d6f3b7aa
carolina <carolina@MONNIE>
2026-05-28 23:36:50 -0400

    finalizacion de la clase padre persona para polimorfismo

--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,29 @@
-public class Persona {
+public abstract class Persona {
     private String Nombre;
     private String apellido;
+    
+    public Persona(String nombre, String apellido) {
+        this.Nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public abstract void MostrarInformacion();
+
+    
 }
