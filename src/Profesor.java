1aa4b7689d4b8eb3650473f471d84c000a33f2ea
carolina <carolina@MONNIE>
2026-05-29 12:32:44 -0400

    finalizacion de clase profesor

--- src/Profesor.java
+++ src/Profesor.java
@@ -2,4 +2,41 @@ public class Profesor extends Persona {
     private int Codigo;
     private String Especialidad;
     
+    public Profesor(String nombre, String apellido, int codigo, String especialidad) {
+        super(nombre, apellido);
+        Codigo = codigo;
+        Especialidad = especialidad;
+    }
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        Codigo = codigo;
+    }
+
+    public String getEspecialidad() {
+        return Especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        Especialidad = especialidad;
+    }
+
+    @Override
+    public void MostrarInformacion(){
+        
+        System.out.println("========================");
+        System.out.println("       INFORMACION:     ");
+        System.out.println("========================");
+        System.out.println("Nombre:" + getNombre());
+        System.out.println("Apellido:" + getApellido());
+        System.out.println("Codigo:" + getCodigo());
+        System.out.println("Especialidad:" + getEspecialidad());
+    
+    }
+
+    
+    
 }
