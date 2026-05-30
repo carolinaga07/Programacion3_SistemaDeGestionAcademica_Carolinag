ad542f76bf01402c20d03239bf4d386572aba8d4
carolina <carolina@MONNIE>
2026-05-30 00:21:09 -0400

    finalizacion de la clase sistema de gestion

--- src/SistemaDeGestion.java
+++ src/SistemaDeGestion.java
@@ -63,5 +63,48 @@ public class SistemaDeGestion {
 
     }
 
+    public void Promedios(){
+        System.out.println("======================");
+        System.out.println(" REPORTE DE PROMEDIOS ");
+        System.out.println("======================");
+
+        for(int i = 0; i < estudiantes.size(); i++){
+            Estudiante e = estudiantes.get(i);
+            double Promedio = e.CalcularPromedio();
+            String Estado = (Promedio >= 60) ? "APROBADO" : "REPROBADO";
+            System.out.printf("%-20s / Promedio: %5.2f  / %s%n" ,
+            e.getNombre() + " " + e.getApellido(), Promedio, Estado);
+            
+        }
+    }
+
+    public void AsignarMateria(String matricula, String codigo){
+        Estudiante e = BuscarPorMatricula(matricula);
+        Materia m = BuscarPorCodigo(codigo);
+
+        if ( e == null || m == null){
+            System.out.println("No encontrado");
+            return;
+        }
+
+        e.AgregarMateria(m);
+        System.out.println("Materia Asignada!!");
+        
+    }
+
+    public void RegistrarCalificacion (String matricula, String codigo, double nota){
+        Estudiante e = BuscarPorMatricula(matricula);
+        Materia m = BuscarPorCodigo(codigo);
+
+        if ( e == null || m == null){
+            System.out.println("No encontrado");
+            return;
+        }
+
+        e.AgregarCalificacion(new Calificacion(m, nota));
+        System.out.println("Calificacion Registrada!!");
+        
+    }
+
 
 }
