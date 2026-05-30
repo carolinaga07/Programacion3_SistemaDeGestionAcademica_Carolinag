a0f3519458abb79dc05517f26149fd10abb022f0
carolina <carolina@MONNIE>
2026-05-29 23:43:38 -0400

    finalizacion de funciones para registar objetos en clases

--- src/SistemaDeGestion.java
+++ src/SistemaDeGestion.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
+
 public class SistemaDeGestion {
+    private ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    private ArrayList<Profesor> profesores = new ArrayList<>();
+    private ArrayList<Materia> materias = new ArrayList<>();
+
+    public void RegistrarE (Estudiante e){
+        estudiantes.add(e);
+        System.out.println("Estudiante Registrado!!");
+    }
+
+    public void RegistrarM (Materia m){
+        materias.add(m);
+        System.out.println("Materia Registrada!!");
+    }
+
+    public void RegistrarP(Profesor p){
+        profesores.add(p);
+        System.out.println("Profesor Registrado!!");
+    }
+
+    public Estudiante BuscarPorMatricula (String matricula){
+
+    }
     
 }
